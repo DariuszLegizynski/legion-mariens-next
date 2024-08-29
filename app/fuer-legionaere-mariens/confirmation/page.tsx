1df7e30b218484f330fe5387667466eaac570e13
Dariusz Legizynski <d.legizynski@.com>
2024-08-29 22:35:29 +0200

    add info to confirmation page about construction

--- app/fuer-legionaere-mariens/confirmation/page.tsx
+++ app/fuer-legionaere-mariens/confirmation/page.tsx
@@ -1,5 +1,10 @@
 const Confirmation = () => {
-	return <div>Confirmation</div>
+	return (
+		<article className="flex flex-col items-center my-24">
+			<h1 className="flex flex-col items-center">Danke für die Bestellung</h1>
+			<p className="mt-12">(Seite im Bau)</p>
+		</article>
+	)
 }
 
 export default Confirmation
