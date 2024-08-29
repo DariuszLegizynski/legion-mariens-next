3fc9e6809b8801c14a9419b3739a2544df4f56ce
Dariusz Legizynski <d.legizynski@.com>
2024-08-29 23:09:31 +0200

    add prayers page

---
+++ app/unsere-spiritualitaet/gebete/page.tsx
@@ -0,0 +1,24 @@
+import { getStrapiData } from "@/app/_utils/services/getStrapiData"
+import PrayerList from "./_components/PrayerList"
+import Separator from "@/components/base/Separator"
+
+const Prayer = async () => {
+	const data = await getStrapiData("spiritualitaet-gebete?populate[content][populate][content][populate]=*")
+	const prayerContentData = data.data.attributes
+
+	console.log({ prayerContentData })
+	return (
+		<article className="mt-24 mx-4">
+			<h1>{prayerContentData.title}</h1>
+
+			{prayerContentData.content?.map((item, index) => (
+				<div className="mt-24" key={`PrayerList_${index}`}>
+					<PrayerList content={item.content} />
+					{index < prayerContentData.content?.length - 1 && <Separator />}
+				</div>
+			))}
+		</article>
+	)
+}
+
+export default Prayer
