52e178e3d0c3d6e9f2f83231c800267e2cd6a29d
Dariusz Legizynski <d.legizynski@.com>
2024-09-18 09:56:19 +0200

    add structure of the legion page

---
+++ app/aufbau/struktur-der-legion-mariens/page.tsx
@@ -0,0 +1,26 @@
+import { getStrapiData } from "@/app/_utils/services/getStrapiData"
+import TitleDescription from "@/components/common/TitleDescription"
+import TitleImage from "@/components/common/TitleImage"
+
+const StrukturLegion = async () => {
+	const organigram = await getStrapiData("aufbau-struktur-der-legion-marien?populate[organigram][populate]=*")
+	const organigramData = organigram.data.attributes.organigram
+
+	const descriptions = await getStrapiData("aufbau-struktur-der-legion-marien?populate[descriptions][populate]=*")
+	const descriptionsData = descriptions.data.attributes
+
+	// console.log({ organigramData, descriptionsData })
+
+	return (
+		<article className="my-24 mx-4 max-container">
+			<TitleImage title={organigramData?.title} image={organigramData?.image} width={815} height={752} />
+			<section className="grid grid-cols-1 md:grid-cols-2 gap-x-8">
+				{descriptionsData.descriptions.map(description => (
+					<TitleDescription key={description.id} title={description.title} description={description.description} />
+				))}
+			</section>
+		</article>
+	)
+}
+
+export default StrukturLegion
---
+++ components/common/TitleImage.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+const TitleImage = ({ title, image, width, height }: { title: string; image: any; width: number; height: number }) => {
+	console.log({ title, image })
+	return (
+		<section className="flex flex-col mb-8">
+			<h1 className="my-2.5">{title}</h1>
+			<Image
+				className={`w-auto object-contain mx-auto my-12`}
+				src={`${process.env.API_URL}${image?.data?.attributes?.url}`}
+				alt={`${process.env.API_URL}/${image?.data?.attributes?.alternativeText}`}
+				width={width}
+				height={height}
+			/>
+		</section>
+	)
+}
+
+export default TitleImage
