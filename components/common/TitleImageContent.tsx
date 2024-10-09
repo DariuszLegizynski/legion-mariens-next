c4edc1c72717983b2793dc316664e912b065c767
Dariusz Legizynski <d.legizynski@.com>
2024-10-09 22:23:15 +0200

    fix legionsbild for large screens

--- components/common/TitleImageContent.tsx
+++ components/common/TitleImageContent.tsx
@@ -7,7 +7,7 @@ const TitleImageContent = ({ title, image, content }: { title: string; image: an
 			<div className="h2 my-2.5">{title}</div>
 			<div>
 				<Image
-					className="h-[400px] object-contain my-12 float-left"
+					className="h-[400px] object-contain my-12 float-left md:h-[560px] lg:h-[640px] md:w-[50%] pr-4"
 					src={`${process.env.API_URL}${image?.data?.attributes?.url}`}
 					alt={`${process.env.API_URL}/${image?.data?.attributes?.alternativeText}`}
 					width={400}
@@ -17,7 +17,7 @@ const TitleImageContent = ({ title, image, content }: { title: string; image: an
 					dangerouslySetInnerHTML={{
 						__html: content?.map((item: Content) => item.children.map((child: Child) => child.text).join("")).join(""),
 					}}
-					className="text-left mt-4 md:text-justify md:pr-8 [&>p]:pt-6"
+					className="max-w-full text-left mt-4 md:text-justify md:pr-8 [&>p]:pt-6"
 				/>
 			</div>
 		</section>
