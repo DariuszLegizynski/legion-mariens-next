becaa8e61e9873afe7d05062165bce94311ae8cd
Dariusz Legizynski <d.legizynski@.com>
2024-10-09 16:13:50 +0200

    add text wrap to legionsbild

--- components/common/TitleImageContent.tsx
+++ components/common/TitleImageContent.tsx
@@ -5,19 +5,21 @@ const TitleImageContent = ({ title, image, content }: { title: string; image: an
 	return (
 		<section className="flex flex-col mb-8">
 			<div className="h2 my-2.5">{title}</div>
-			<Image
-				className="h-80 w-full object-contain mx-auto my-12"
-				src={`${process.env.API_URL}${image?.data?.attributes?.url}`}
-				alt={`${process.env.API_URL}/${image?.data?.attributes?.alternativeText}`}
-				width={600}
-				height={400}
-			/>
-			<div
-				dangerouslySetInnerHTML={{
-					__html: content?.map((item: Content) => item.children.map((child: Child) => child.text).join("")).join(""),
-				}}
-				className="text-left mt-4 grid gap-y-4"
-			/>
+			<div>
+				<Image
+					className="h-[400px] object-contain my-12 float-left"
+					src={`${process.env.API_URL}${image?.data?.attributes?.url}`}
+					alt={`${process.env.API_URL}/${image?.data?.attributes?.alternativeText}`}
+					width={400}
+					height={300}
+				/>
+				<div
+					dangerouslySetInnerHTML={{
+						__html: content?.map((item: Content) => item.children.map((child: Child) => child.text).join("")).join(""),
+					}}
+					className="text-left mt-4 md:text-justify md:pr-8 [&>p]:pt-6"
+				/>
+			</div>
 		</section>
 	)
 }
