c904becedf32d05b25c0ab0d43a8f85a0ae94f05
Dariusz Legizynski <d.legizynski@.com>
2024-10-23 20:54:17 +0200

    add new event btn to termine

--- app/termine/_components/EventCalendar.tsx
+++ app/termine/_components/EventCalendar.tsx
@@ -1,4 +1,7 @@
 "use client"
+import { useEffect, useState } from "react"
+import Link from "next/link"
+
 import type { Category } from "@/types/Category"
 
 import DatePicker, { registerLocale, setDefaultLocale } from "react-datepicker"
@@ -6,6 +9,8 @@ import { de } from "date-fns/locale/de"
 registerLocale("de", de)
 setDefaultLocale("de")
 import "react-datepicker/dist/react-datepicker.css"
+
+import Cookies from "js-cookie"
 
 type SetCategoryFn = (value: string) => void
 type SetStartDateFn = (value: string) => void
@@ -36,9 +41,16 @@ const EventCalendar = ({
 	assignments: Category[]
 	setAssignment: SetAssignmentFn
 }) => {
+	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
+
+	const isCookie = Cookies.get("jwt")
+	useEffect(() => {
+		setIsAuthenticated(Cookies.get("jwt") ? true : false)
+	}, [isCookie])
+
 	return (
 		<>
-			<section className="grid grid-cols-1 justify-items-center gap-4 mt-8 mb-4 md:gap md:grid-cols-[1fr_1fr_1fr] md:gap-x-8 md:justify-between mx-4">
+			<section className="grid grid-cols-1 justify-items-center gap-4 mt-8 mb-4 md:gap md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] md:gap-x-8 md:justify-between mx-4">
 				<select onChange={e => setCategory(e.target.value)}>
 					<option value="Alle Kategorien">Alle Kategorien</option>
 					{categories.length > 0 &&
@@ -57,7 +69,7 @@ const EventCalendar = ({
 							</option>
 						))}
 				</select>
-				<select onChange={e => setAssignment(e.target.value)}>
+				<select onChange={e => setAssignment(e.target.value)} className="md:col-span-2 lg:col-auto">
 					<option value="Beides">Beides</option>
 					{assignments.length > 0 &&
 						assignments.map(assignment => (
@@ -67,7 +79,7 @@ const EventCalendar = ({
 						))}
 				</select>
 			</section>
-			<section className="grid grid-cols-1 gap-4 mt-4 mx-4 mb-8 md:gap md:grid-cols-[1fr_1fr_1fr] md:gap-x-8 md:justify-between">
+			<section className="grid grid-cols-1 gap-4 mt-4 mx-4 mb-8 md:gap md:grid-cols-[1fr_1fr] lg:grid-cols-[1fr_1fr_1fr] md:gap-x-8 md:justify-between">
 				<DatePicker
 					selected={startDate}
 					locale="de"
@@ -84,6 +96,14 @@ const EventCalendar = ({
 					dateFormat="dd MMMM yyyy"
 					placeholderText="Enddatum wählen"
 				/>
+				{isAuthenticated && (
+					<Link
+						href="/fuer-legionaere-mariens/neuer-termin"
+						className="md:col-span-2 lg:col-auto bg-green text-center rounded cursor-pointer hover:scale-105 hover:bg-green-dark focus:scale-105 focus:bg-green-dark transition-all ease-in-out"
+					>
+						<p className="text-white py-2">Neuer Termin</p>
+					</Link>
+				)}
 			</section>
 		</>
 	)
